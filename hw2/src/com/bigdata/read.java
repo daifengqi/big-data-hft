a3abce2eb262bc1a07eb6d81faa1267cdbf50c7e
Cedric Qi <daifengqi@stu.pku.edu.cn>
2021-12-21 10:20:35 +0800

    hadoop package add to library

---
+++ hw2/src/com/bigdata/read.java
@@ -0,0 +1,13 @@
+package com.bigdata;
+
+import org.apache.hadoop.io.IntWritable;
+
+public class read {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Hello Java!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
