61b6415e7b01297be68c6fe3a46ab99b4facec40
Cedric Qi <daifengqi@stu.pku.edu.cn>
2021-12-26 12:39:46 +0800

    format

--- hw_2/src/main/java/DataAnalysisMapper.java
+++ hw_2/src/main/java/DataAnalysisMapper.java
@@ -1,6 +1,8 @@
 // The following is a map process
+
 import java.io.IOException;
 import java.util.StringTokenizer;
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -15,6 +17,7 @@ import org.apache.hadoop.yarn.webapp.hamlet2.Hamlet;
 
 public class DataAnalysisMapper extends Mapper<Object, Text, Text, IntWritable> {
     private final static IntWritable one = new IntWritable(1);
+
     public void map(Object key, Text value, OutputCollector<Text, IntWritable> output) throws IOException, InterruptedException {
         // 数据预处理：如果是表头（key是0），不处理；如果PM2.5的值是Nan，不处理；直接返回
         if (key.toString().equals("0"))
@@ -25,29 +28,27 @@ public class DataAnalysisMapper extends Mapper<Object, Text, Text, IntWritable> 
             return;
 
         // For Q1：记录output的key：“年_月_日_小时_good(medium/bad)”
-        String time = String.format("%s_%s_%s_%s",oneRecord[1].toString(),oneRecord[2].toString(),oneRecord[3].toString(),oneRecord[4].toString());
+        String time = String.format("%s_%s_%s_%s", oneRecord[1].toString(), oneRecord[2].toString(), oneRecord[3].toString(), oneRecord[4].toString());
         String station = oneRecord[17];
         Double PM25 = Double.valueOf(oneRecord[5].toString());
-        Text goodTime = new Text(time+"_good");
-        Text mediumTime = new Text(time+"_medium");
-        Text badTime = new Text(time+"_bad");
+        Text goodTime = new Text(time + "_good");
+        Text mediumTime = new Text(time + "_medium");
+        Text badTime = new Text(time + "_bad");
 
         //For Q2：记录output的key，“station_fine”
-        Text stationFine = new Text(station+"_fine");
-        Text stationBad = new Text(station+"_bad");
+        Text stationFine = new Text(station + "_fine");
+        Text stationBad = new Text(station + "_bad");
 
         //对雨PM25的值进行判断，将对应Q1 Q2的output写入context中
-        if (PM25 <= 35){
-            output.collect(goodTime,one);
-            output.collect(stationFine,one);
-        }
-        else if (PM25 <= 75){
-            output.collect(mediumTime,one);
-            output.collect(stationFine,one);
-        }
-        else{
-            output.collect(badTime,one);
-            output.collect(stationBad,one);
+        if (PM25 <= 35) {
+            output.collect(goodTime, one);
+            output.collect(stationFine, one);
+        } else if (PM25 <= 75) {
+            output.collect(mediumTime, one);
+            output.collect(stationFine, one);
+        } else {
+            output.collect(badTime, one);
+            output.collect(stationBad, one);
         }
     }
 }
--- hw_2/src/main/java/DataAnalysisReducer.java
+++ hw_2/src/main/java/DataAnalysisReducer.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -12,23 +13,21 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.yarn.webapp.hamlet2.Hamlet;
 
-public class DataAnalysisReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+public class DataAnalysisReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
     public void reduce(Text key, Iterable<IntWritable> values, OutputCollector<Text, Double> output) throws IOException {
         // 计算总数
         double sum = 0;
         double numCity = 12;
         double result;
 
-
-        for (IntWritable value: values){
+        for (IntWritable value : values) {
             sum += value.get();
         }
         // 对于不同的key进行计数: Q1的key中含有数字，Q2的key中不含数字
-        if (Character.isDigit(key.toString().charAt(0))){
+        if (Character.isDigit(key.toString().charAt(0))) {
             // 对于Q1：除以city的数量得到ratio
             result = sum / numCity;
-            }
-        else{
+        } else {
             // 对于Q2，直接就是计数的sum
             result = sum;
         }
